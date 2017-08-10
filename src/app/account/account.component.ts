dd463effc5c73599284f2c30e9150796488004ec
poyal <ttt4504@gmail.com>
2017-08-10 10:06:39 +0900

    service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,23 +1,20 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {LoggingService} from '../logging.service';
+import {Component, Input} from '@angular/core';
 import {AccountService} from '../account.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService, AccountService]
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
   @Input() account: { name: string, status: string };
   @Input() id: number;
   
-  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+  constructor(private accountService: AccountService) {
   
   }
   
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,21 +1,18 @@
 import {Component} from '@angular/core';
-import {LoggingService} from '../logging.service';
 import {AccountService} from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
   
-  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+  constructor(private accountService: AccountService) {
   
   }
   
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
